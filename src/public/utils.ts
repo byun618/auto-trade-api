5b1905bcbd46e38bc2cb307b42553ab0f4096683
byun618 <byun618@gmail.com>
2022-03-27 03:11:55 +0900

    개발 디비 개발

--- src/public/utils.ts
+++ src/public/utils.ts
@@ -3,8 +3,12 @@ import mongoose from 'mongoose'
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
 export const connectMongoDb = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}/auto-trade`,
-  )
+  if ((process.env.VERSION as string) !== 'DEV') {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}/auto-trade`,
+    )
+  } else {
+    await mongoose.connect('mongodb://localhost:27017/auto-trade')
+  }
   console.log('mongoDB connected')
 }
